f8be3ca2f73b901929ce4924e555d3f458e0073b
uqjeadi2 <jack.eadie0@gmail.com>
2017-06-30 14:18:38 +1000

    File reformatting considered. Usage, test set up and results included

--- Performance_Improvement.py
+++ Performance_Improvement.py
@@ -3,8 +3,9 @@ import h5py
 import matplotlib
 import os
 from math import ceil
-#from scipy import ndimage
-from timeit import timeit
+
+from scipy import ndimage
+import timeit as T
 """
 General Page to store Ideas and methods to improve performance of SlideCrop backEnd. 
 
@@ -23,27 +24,9 @@ Alot of ram access between for tiff files and storing alot of the datasets on RA
 #  TODO: Test time requirements to transform .ims file to (x,y,c) data set arrays to improve performance
 #  TODO: Work on compression ideas and operating on compressed h5py files
 
-
+FILECOUNT = 1
 FILEPATH = "C:/Users/uqjeadi2/Downloads/NG_GAD67_GFP16-B.ims"
-
+STOREFILEPATH = "C:/Users/uqjeadi2/Downloads/"
 def import_hdf_File():
     return h5py.File(FILEPATH)
 
-def hdf_data_from_Resolution(resLev, h5py, chan=None):
-    if not chan:
-        return h5py["DataSet/ResolutionLevel " + str(resLev) + "/TimePoint 0/Channel 0/Data"]
-
-    return h5py["DataSet/ResolutionLevel " + str(resLev) + "/TimePoint 0/Channel " + str(chan) + "/Data"]
-
-def main():
-    print(timeit(import_hdf_File))
-    file = import_hdf_File()
-    # print("channel 0" + str(timeit(hdf_data_from_Resolution(0, file))))
-    # print("channel 1" + str(timeit(hdf_data_from_Resolution(1, file))))
-    # print("channel 2" + str(timeit(hdf_data_from_Resolution(2, file))))
-    # print("channel 3" + str(timeit(hdf_data_from_Resolution(3, file))))
-    # print("channel 4" + str(timeit(hdf_data_from_Resolution(4, file))))
-    # print("channel 5" + str(timeit(hdf_data_from_Resolution(5, file))))
-
-if __name__ == '__main__':
-    main()
