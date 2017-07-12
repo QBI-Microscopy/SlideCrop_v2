7ec375b17f3e92d8ae89ef3a3d7ee4bc9c0e40eb
Jeadie <jack.eadie0@gmail.com>
2017-07-12 18:36:35 +1000

    High Level API memory testing

--- High_Level_API_Test.py
+++ High_Level_API_Test.py
@@ -10,11 +10,15 @@
 import h5py
 import numpy as np
 import random
-import memory_profiler
+from  memory_profiler import *
+from line_profiler import *
+from timeit import Timer
+
 
 FILEDIRECTORY = "E:/"
 MAINFILEPATH = FILEDIRECTORY + "trial.hdf"
-EXISTING_FILE = "C:/Users/uqjeadi2/Downloads/NG_GAD67_GFP16-B.ims"
+EXISTING_FILE = "E:/NG_GAD67_GFP16-B.ims"
+USED_PATH = FILEDIRECTORY + "empty" + str(random.getrandbits(10)) # + ".hdf"
 
 @profile
 def opening_and_creating_files():
@@ -30,40 +34,60 @@ def opening_and_creating_files():
     #Close Second
     exisiting_file.close()
 
+@profile
 def create_dataset():
-    file = h5py.File(FILEDIRECTORY + "empty" + str(random.getrandbits(10)) + ".hdf")
+    file = h5py.File(USED_PATH + str(random.getrandbits(8)) + ".hdf")
 
     # Create standard, random dataset
-    r_set  = np.random.randint(0, 1000, size=(100,100, 100), dtype='u')
-    file.create_dataset("standard", (100, 100, 100), dtype = 'u', data = r_set)
+    r_set  = np.random.randint(0, 1000, size=(200,200, 200), dtype='i')
+    file.create_dataset("standard", (200,200, 200), dtype = 'i', data = r_set)
 
     # float dataset
-    f_set = np.random.rand(100, 100, 100)
-    file.create_dataset("float", (100, 100, 100), dtype='f', data=f_set)
+    f_set = np.random.rand(200,200, 200)
+    file.create_dataset("float", (200,200, 200), dtype='f', data=f_set)
 
     #Create New Group
-    file.create_group("subgroup")
+    sub = file.create_group("subgroup")
     # chunked dataset
-    c_set = np.random.randint(0, 1000, size=(100,100, 100), dtype='u')
-    file.get("subgroup").create_dataset("chunked", (100,100,100), dtype='u', data= c_set, chunks=(10,10,10))
+    c_set = np.random.randint(0, 1000, size=(200,200, 200), dtype='i')
+    sub.create_dataset("chunked", (200,200, 200), dtype='i', data= c_set, chunks=(10,10,10))
 
     #Create Nested Group
-    # Resizeable Dataset
+    nest = sub.create_group("nested")
+
     # Compressed Dataset
-    pass
+    nest.create_dataset("compressed", (200,200, 200), dtype= 'i', data= r_set, compression="gzip", compression_opts=9)
+
 
 #  Typical operations when performing analysis of IMS file on SlideCrop
+@profile
 def slicing_and_operating_datasets():
-    #Slicing 1D data
+    file = h5py.File(USED_PATH)
 
-    #Slicing 2+D dataset
+    #Slicing sequential data
+    r_set= file.get("standard")
+    slicer1 = r_set[0,0,:]
+    slicer2 = r_set[0,:,0]
+    slicer3 = r_set[:,0,0]
+    slicer4 = r_set[[1,2],1:4,1:4]
+    slicer5 = r_set[1:4,1:4, [1,2]]
 
-    # Slicing chunked Data
+    # Slicing compressed Data
+    c_set = file.get("subgroup/nested/compressed")
+    slicec1 = c_set[0, 0, :]
+    slicec2 = c_set[0, :, 0]
+    slicec3 = c_set[:, 0, 0]
+    slicec4 = c_set[[1, 2], 1:4, 1:4]
+    slicec5 = c_set[1:4, 1:4, [1, 2]]
 
-    #Slicing compressed Data
+    #Slicing chunked Data
+    ch_set = file.get("subgroup/chunked")
+    slicech1 = ch_set[0, 0, :]
+    slicech2 = ch_set[0, :, 0]
+    slicech3 = ch_set[:, 0, 0]
+    slicech4 = ch_set[[1, 2], 1:4, 1:4]
+    slicech5 = ch_set[1:4, 1:4, [1, 2]]
 
-    #Resizing datasets
-    pass
 
 #  Typical operations for saving and sending data into .tiff file formats
 def tiff_and_ims_operations():
@@ -77,16 +101,9 @@ def groups_iteration():
 def cropping_IMS_to_hdf():
     pass
 
-@profile
-def my_func():
-    a = h5py.File(FILEDIRECTORY + str(random.getrandbits(10))+ "hdf5")
-
-
 
 if __name__ == '__main__':
-    opening_and_creating_files()
-    create_dataset()
-    slicing_and_operating_datasets()
-    tiff_and_ims_operations()
-    groups_iteration()
-    cropping_IMS_to_hdf()
+    print(str(Timer(lambda: create_dataset()).timeit(5)))
+    # lp= LineProfiler()
+    # lp_wrapper= lp(create_dataset)
+    # lp.print_stats()
