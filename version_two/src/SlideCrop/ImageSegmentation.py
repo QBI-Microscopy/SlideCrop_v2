6fda5cdd36861a6595cd4e5c84378834e9bf540f
uqjeadi2 <jack.eadie0@gmail.com>
2017-08-11 16:46:00 +1000

    ImageSegmentationTest implemented, ImageSegmentation under development

--- version_two/src/SlideCrop/ImageSegmentation.py
+++ version_two/src/SlideCrop/ImageSegmentation.py
@@ -3,8 +3,10 @@ class ImageSegmentation(object):
     Data Structure to hold box segments for images. Box segments are defined by two points: 
     upper-left & bottom-right.  
     """
-    def __init__(self):
-        pass
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        self.segments = []
 
     def add_segmentation(self, x1, y1, x2, y2):
         """
@@ -13,7 +15,13 @@ class ImageSegmentation(object):
         :param x2, y2: bottom-right point of the segment box
         :return: null
         """
-        pass
+        if (any(value < 0 for value in [x1, y1, x2, y2]) |
+           any(x > self.width for x in [x1, x2])  |
+           any(y > self.height for y in [y1, y2])):
+           raise InvalidSegmentError
+        else:
+            self.segments.append([x1, y1, x2, y2])
+
 
     def get_scaled_segments(self, width, height):
         """
@@ -29,3 +37,6 @@ class ImageSegmentation(object):
         """
         pass
 
+
+class InvalidSegmentError(Exception):
+    pass
