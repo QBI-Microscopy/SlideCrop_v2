6f9549bc780ff8b13f5046358d2dc27f1e9dc073
uqjeadi2 <jack.eadie0@gmail.com>
2017-08-28 14:50:35 +1000

    Histogram_thresholding_algorithm implemented K means clustering algorithm using histogram construct. Reduces time complexity significantly

--- version_two/src/SlideCrop/K_means_threshold_clustering.py
+++ version_two/src/SlideCrop/K_means_threshold_clustering.py
@@ -5,26 +5,26 @@ def closest_index(ndarray, value):
 import scipy.ndimage as nd
 import numpy as np
 import matplotlib.pyplot as plt
-K_Clusters = 5
+K_Clusters = 10
 BGPCOUNT = 40
 SENSITIVITY_THRESHOLD = 0.01
-image = nd.imread("E:/test1.jpg")
+image = nd.imread("E:/test2.jpg")
 x_dim = image.shape[0]
 y_dim = image.shape[1]
 
 if image.ndim == 3:
-    # mean intensity for each channel
+    # Find mean intensity for each channel
     # Averaged across axis separately. Shape = (x,y,3) -> (x -> 3) -> (1,3)
     channel_mean_vector  = np.mean(np.mean(image, axis =1),axis =0 )
 
-    #  Find x & y indices to for the four corner squares of size BGPCOUNT * BGPCOUNT
+    #  Find x & y indices for the four corner squares of size BGPCOUNT * BGPCOUNT
     background_index_x_list = []
     background_index_y_list = []
     for i in range(BGPCOUNT):
         background_index_x_list.append(i);
         background_index_y_list.append(i);
-        background_index_x_list.append(x_dim - (i+1));
-        background_index_y_list.append(y_dim - (i+1));
+        background_index_x_list.append(x_dim - (i + 1));
+        background_index_y_list.append(y_dim - (i + 1));
 
     # Create a image vector with pixel values from the 2D corners of all channels. Used as a background intensity mean.
     background_vector = image[background_index_x_list, background_index_y_list, :];
@@ -32,39 +32,37 @@ if image.ndim == 3:
     background_channel_mean_vector = np.mean(np.mean(background_vector, axis =1),axis =0 )
 
 
-    #Choose channel for clustering based on maximum background-average intensity difference.
+    #Choose channel for clustering based on maximum difference in background and average intensity
     max_difference_channel = np.abs(background_channel_mean_vector - channel_mean_vector)
     clustering_channel = np.argmax(max_difference_channel)
     channel_image = image[:,:,clustering_channel]
 else:
+    # if 2D, just choose the only channel possible
     channel_image = image.copy()
-index_image = -1 * np.ones((x_dim, y_dim))
-print("Image Shape: {}".format(channel_image.shape))
-print("Index Shape: {}".format(index_image.shape))
+
+# Iterate through pixels of image and use histogram as tally where index is the intensity 0-255 inclusive
+histogram = np.zeros((256))
+index_histogram = -1 * np.ones((256))
+for pixel in channel_image:
+    histogram[pixel] +=1
 
 # Initiate k clusters equidistant on the domain of the channel intensity
 cluster_vector = np.linspace(np.min(channel_image), np.max(channel_image), num = K_Clusters)
 cluster_temp_vector = cluster_vector.copy()
+cluster_vector_totals = np.zeros(K_Clusters)
+cluster_vector_count = np.zeros(K_Clusters)
 
-print("min {}, max{}, {}".format(np.min(channel_image) ,np.max(channel_image), cluster_temp_vector))
-i = 0
 while(1):
-    i+= 1
-    # For each pixel find which cluster in cluster_vector is closest
-    for row in range(channel_image.shape[0]):
-        for col in range(channel_image.shape[1]):
-            index_image[row, col] = closest_index(cluster_vector,
-                                                  channel_image[row, col])
-    #  Find new average for each cluster based on the mean of those in the cluster.
+
+    # Find closest cluster for each pixel intensity in histogram
+    index_histogram = [closest_index(cluster_vector, i) for i in range(256)]
+
+    # for each cluster, find mean of clustered pixel intensities
+    # histogram[i] is number of pixels at intensity i
     for k in range(K_Clusters):
-        cluster_temp_vector[k] = np.mean(channel_image[np.where(index_image == k)])
-
-
-    # print("\nIteration {}".format(i))
-    # print("cluster_vector: {}".format(cluster_vector))
-    print("cluster_temp_vector: {}".format(cluster_temp_vector))
-    # print("diff: {}".format((np.abs(cluster_vector/cluster_temp_vector - 1))))
-    # print("end: {}".format((np.abs(cluster_vector/cluster_temp_vector - 1) < SENSITIVITY_THRESHOLD).all()))
+        weighted_mean_sum = sum(ind * histogram[ind] for ind in range(256) if index_histogram[ind] == k)
+        pixel_count = sum(histogram[ind] for ind in range(256) if index_histogram[ind] == k)
+        cluster_temp_vector[k] = weighted_mean_sum / pixel_count
 
     #  If all clusters change less than the threshold -> finish iteration
     if (np.abs(cluster_vector/cluster_temp_vector - 1) < SENSITIVITY_THRESHOLD).all():
@@ -73,6 +71,4 @@ while(1):
     cluster_vector = cluster_temp_vector.copy()
 
 
-plt.imshow(index_image)
-plt.show()
-
+print("Final Cluster Intensities: {}".format(cluster_vector))
